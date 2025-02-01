0ce62c8d2808ce0f08de0ddf44b33c9157d876fa
YojeetKashyap <yojit1971@gmail.com>
2025-02-01 11:12:16 +0530

    Update news_api.py

--- news_api.py
+++ news_api.py
@@ -56,7 +56,8 @@ def news():
         
     return jsonify({"articles":articles,"totalResults":results})
             
-
+def handler(event, context):
+    return app(event, context)
 
 if __name__ == "__main__":
     app.run(debug=True)
