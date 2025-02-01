2703cdf6b8afd219febdda432c06396164b64d74
YojeetKashyap <yojit1971@gmail.com>
2025-02-01 11:25:45 +0530

    Update news_api.py

--- news_api.py
+++ news_api.py
@@ -1,8 +1,10 @@
 from flask import Flask,jsonify,request
+from flask cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app)
 @app.route("/")
 def news():
 
